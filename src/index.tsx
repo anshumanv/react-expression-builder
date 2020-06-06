29d5a97747c799117a0ef6b2e9d2fc414675625d
Anshuman Verma <anshu.av97@gmail.com>
2020-06-06 21:31:54 +0530

    feat: apply own root node, less work for user

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,17 @@
+import React, { FC } from 'react'
 import Dropdown from './components/Dropdown'
+import { EditorState, TreeNode } from './utils/DataStructure'
+import { EditorType, ExtractorProps } from './utils/Types'
 
-export { EditorState, TreeNode } from './components/DataStructure'
+export { EditorState, TreeNode } from './utils/DataStructure'
 
-export default Dropdown
+const ExpressionBuilder: FC<ExtractorProps> = props => {
+	// create a root node for the component
+	const rootNode = new TreeNode(null)
+	// initialize the editor state with root node
+	const EditorData: EditorType = new EditorState(rootNode)
+
+	return <Dropdown {...props} node={rootNode} EditorData={EditorData} />
+}
+
+export default ExpressionBuilder
